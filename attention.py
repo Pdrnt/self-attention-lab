e057618cb919a2213d47156c87873edf529647f6
Pedro <pedrocarvalholima2015@gmail.com>
2026-02-16 19:04:36 -0300

    feat: adiciona scaling factor sqrt(d_k)

--- attention.py
+++ attention.py
@@ -4,7 +4,11 @@ import numpy as np
 class ScaledDotProductAttention:
 
     def forward(self, Q, K, V):
-        # 1️⃣ Produto escalar entre Q e K^T
+        # 1️⃣ Produto escalar QK^T
         scores = np.matmul(Q, K.T)
 
-        return scores
+        # 2️⃣ Scaling factor
+        d_k = K.shape[-1]
+        scaled_scores = scores / np.sqrt(d_k)
+
+        return scaled_scores
