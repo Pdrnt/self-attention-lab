23f84ea9dd50c68ede96ff55b254d434e2b33262
Pedro <pedrocarvalholima2015@gmail.com>
2026-02-16 18:52:23 -0300

    feat: implementa cálculo do produto escalar QK^T

--- attention.py
+++ attention.py
@@ -4,4 +4,7 @@ import numpy as np
 class ScaledDotProductAttention:
 
     def forward(self, Q, K, V):
-        pass
+        # 1️⃣ Produto escalar entre Q e K^T
+        scores = np.matmul(Q, K.T)
+
+        return scores
